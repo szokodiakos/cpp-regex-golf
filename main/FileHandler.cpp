80d50265edc9aa369ca36a4e0c1c7a1e234f81e7
Szokodi Ákos <szokodiakos@gmail.com>
2015-04-19 18:13:58 +0200

    Filehandler directory item listing added

--- main/FileHandler.cpp
+++ main/FileHandler.cpp
@@ -1,5 +1,8 @@
-#include "FileHandler.h"
 #include <iostream>
+#include <memory>
+#include <cstring>
+#include <dirent.h>
+#include "FileHandler.h"
 
 FileHandler::FileHandler() {
 }
@@ -8,7 +11,15 @@ FileHandler::~FileHandler() {
 }
 
 std::vector<std::string> FileHandler::getFileNames(std::string directory) {
-    std::vector<std::string> v;
-    std::cout << "getFileName" << std::endl;
-    return v;
+    std::shared_ptr<DIR> dir(opendir(directory.c_str()));
+    std::vector<std::string> files;
+    if (dir != nullptr) {
+        while (auto ep = readdir(dir.get())) {
+            auto fileName = ep->d_name;
+            if ((std::strcmp(fileName, ".") != 0) && (std::strcmp(fileName, "..") != 0)) {
+                files.push_back(fileName);
+            }
+        }
+    }
+    return files;
 }
