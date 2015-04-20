4f87cb1fee9420052a52fb5fa06a946d73bb04ba
Szokodi Ákos <szokodiakos@gmail.com>
2015-04-20 15:07:02 +0200

    added memory leak testing

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,57 @@
 #include <string>
 #include "Game.h"
 
+// memory leak checking
+int reserved = 0;
+int silentAllocation = true;
+
+void * operator new(size_t size) {
+    if(size > 1024*1024) throw std::bad_alloc();
+    void *res = malloc(size);
+    if(res == nullptr) throw std::bad_alloc();
+    if (!silentAllocation) {
+        std::cout << "~~~ allocated " << size << " bytes at " << res << std::endl;
+    }
+    reserved ++;
+    return res;
+}
+
+void operator delete(void *p) noexcept {
+    free(p);
+    if (!silentAllocation) {
+        std::cout << "~~~ freed memory at " << p << std::endl;
+    }
+    reserved--;
+}
+
+void * operator new[](size_t size) {
+    if(size > 1024*1024) throw std::bad_alloc();
+    void *res = malloc(size);
+    if(res == nullptr) throw std::bad_alloc();
+    if (!silentAllocation) {
+        std::cout << "~~~ allocated " << size << " bytes at " << res << std::endl;
+    }
+    reserved ++;
+    return res;
+}
+
+void operator delete[](void *p) noexcept {
+    free(p);
+    if (!silentAllocation) {
+        std::cout << "~~~ freed memory at " << p << std::endl;
+    }
+    reserved--;
+}
+
+void check() {
+    if (!silentAllocation) {
+        std::cout << "~~~ Reserved: " << reserved << std::endl;
+    }
+}
+
 int main(int argc, char **argv) {
+    atexit(check);
+
     std::cout << R"(  ____                         ____       _  __ )" << std::endl;
     std::cout << R"( |  _ \ ___  __ _  _____  __  / ___| ___ | |/ _|)" << std::endl;
     std::cout << R"( | |_) / _ \/ _` |/ _ \ \/ / | |  _ / _ \| | |_ )" << std::endl;
@@ -13,6 +63,9 @@ int main(int argc, char **argv) {
     std::string input;
     game.print();
     while (getline(std::cin, input)) {
+        if (input == "*q") {
+            break;
+        }
         game.input(input);
         game.print();
     }
